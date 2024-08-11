0b0003cc4a51aa1f26c1f44b54cccb61085918f7
dmitriy <kylishovdimka@ukr.net>
2024-08-11 20:45:30 +0300

    add in groups. py - ClassRoom and work is admin

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin # noqa
+from django.contrib import admin
+
+from groups.models import ClassRoom, Group
+from students.models import Student
+
 
 # Register your models here.
+
+
+class StudentTable(admin.TabularInline):
+    model = Student
+    fields = ['first_name', 'last_name', 'email', 'rating']
+    readonly_fields = fields
+    show_change_link = True
+
+
+class GroupAdmin(admin.ModelAdmin):
+    # list_display = ['name', 'course', 'head']
+    # fields = ['name', 'course', 'head', 'classrooms']
+    inlines = [StudentTable]
+
+
+admin.site.register(ClassRoom)
+admin.site.register(Group, GroupAdmin)
--- groups/models.py
+++ groups/models.py
@@ -1,14 +1,34 @@
 import datetime
 
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+
+class ClassRoom(models.Model):
+    name = models.CharField(max_length=32, null=True)
+    floor = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(4)])
+
+    def __str__(self):
+        return f"{self.name}, floor {self.floor}"
 
 
 class Group(models.Model):
     name = models.CharField(max_length=64, null=False)
     course = models.CharField(max_length=64, null=True)
     start_date = models.DateField(null=True, default=datetime.date.today)
+
+    head = models.OneToOneField(
+        to='students.Student',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='head_of_group'
+    )
+
+    classrooms = models.ManyToManyField(
+        to=ClassRoom,
+        related_name='groups'
+    )
 
     def __str__(self):
         return f"{self.name}, {self.course}, {self.start_date}"
