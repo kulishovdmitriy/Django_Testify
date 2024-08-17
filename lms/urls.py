f87d9608d7fcb9f55a71a6986f4c73e197946397
dmitriy <kylishovdimka@ukr.net>
2024-08-17 17:20:15 +0300

    add Profile, UserAction in admin.py, add MEDIA

--- lms/urls.py
+++ lms/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -34,6 +34,9 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 
 ]
+
+urlpatterns += \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
