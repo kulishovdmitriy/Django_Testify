d7ecea9acf83fc99d786597e77011dfb44a1556f
dmitriy <kylishovdimka@ukr.net>
2024-08-17 16:31:29 +0300

    Add models UserAction, Profile. Inst Pillow

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
@@ -22,7 +23,14 @@ class AccountLoginView(LoginView):
     template_name = "login.html"
 
     def get_redirect_url(self):
+        if self.request.GET.get("next"):
+            return self.request.GET.get("next")
         return reverse("core:index")
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        messages.info(self.request, f'User {self.request.user} has been successfully logged in')
+        return result
 
 
 class AccountLogoutView(LogoutView):
